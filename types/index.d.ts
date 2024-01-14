0efd2e316c854f076eedab4beadd6171eb5f8e7f
Esteban Sepulveda <tebansd@icloud.com>
2024-01-14 11:47:49 -0800

    final solution

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,3 +1,7 @@
 declare module 'lodash' {
-    
+    function multiply(multiplier: number, multiplicand: number): number
+
+    function reverse<Type>(array:Type[]): Type[]
+
+    function dropRight(array: (string | number)[], n: number): (string | number)[]
 }
